3fdd481d2aa11b8552e4a742d570ef2ac5858b02
gubterra <gubterra@hotmail.com>
2014-10-12 21:48:41 -0300

    commit prog2
    commit programming assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+makeCacheMatrix <- function( mat = matrix() ) {
+  
+  ## Init var
+    inv <- NULL
+  
+  set <- function( matrix ) {
+  mat <<- matrix
+  inv <<- NULL
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  get <- function() {
+    mat
+  }
+  
+  ##  set inverse of the matrix
+  setInverse <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  ## get inverse of the matrix
+  getInverse <- function() {
+    ## Return inv 
+    inv
+  }
+  
+  ## Return a list of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix inverse of 'x'
+  mat <- x$getInverse()
+  
+  ## Return the inverse if its set
+  if( !is.null(mat) ) {
+    message("caching...")
+    return(mat)
+  }
+     
+  data <- x$get()
+    
+  mat <- solve(data) %*% data
+    
+  x$setInverse(mat)
+  
+  ## Return matrix
+  mat
 }
